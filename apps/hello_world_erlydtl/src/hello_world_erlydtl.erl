3c80f256c8b8c7bb9bdcb4f0c1e94b8995c06f2c
Steve Song <songs@maplekeycompany.com>
2012-03-23 20:40:53 -0700

    Using spacing from other examples instead of default Emacs pretty-printing

--- apps/hello_world_erlydtl/src/hello_world_erlydtl.erl
+++ apps/hello_world_erlydtl/src/hello_world_erlydtl.erl
@@ -5,20 +5,16 @@ start() ->
     application:start(cowboy),
     application:start(hello_world_erlydtl).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
 start(_Type, _Args) ->
     Dispatch = [
-		{'_', [
-		       {'_', hello_world_erlydtl_handler, []}
-		      ]}
-	       ],
+        {'_', [
+            {'_', hello_world_erlydtl_handler, []}
+        ]}
+    ],
     cowboy:start_listener(my_http_listener, 1,
-			  cowboy_tcp_transport, [{port, 8080}],
-			  cowboy_http_protocol, [{dispatch, Dispatch}]
-			 ).
+        cowboy_tcp_transport, [{port, 8080}],
+        cowboy_http_protocol, [{dispatch, Dispatch}]
+    ).
 
 stop() ->
     application:stop(cowboy).
